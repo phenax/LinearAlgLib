4dcdc1bb395c7669b2c13960b60d0f8a7866be78
AdiPat <adipat247360@gmail.com>
2016-12-12 20:02:28 +0530

    Add basic vector operations

--- vector.py
+++ vector.py
@@ -19,3 +19,37 @@ class Vector(object):
 
     def __eq__(self, v):
         return self.coordinates == v.coordinates
+
+    # Adds vector v with itself and returns a vector that is the sum of both 
+
+    def plus(self, v):
+        new = [] 
+        n = len(self.coordinates)
+        for i in range(n):
+            new.append(self.coordinates[i] + v.coordinates[i])
+        return Vector(new) 
+
+    # Subtracts vector v from vector self and returns the vector that is the difference of both
+
+    def minus(self, v):
+        new = [] 
+        n = len(self.coordinates)
+        for i in range(n):
+            new.append(self.coordinates[i] - v.coordinates[i])
+        return Vector(new)
+
+
+    def sc_mult(self, c):
+        new = []
+        n = len(self.coordinates)
+        for i in range(n):
+            new.append(self.coordinates[i] * c) 
+        return Vector(new)
+
+# Test code, will be removed soon 
+
+v1 = Vector([-1,2,3])
+v2 = Vector([1,2,3])
+print v1.plus(v2)
+print v1.minus(v2)
+print v1.sc_mult(3) 
